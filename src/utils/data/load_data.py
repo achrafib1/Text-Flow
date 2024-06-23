3c82bcf8bac788f9f7278d768d2bf392ad747567
achrafib1 <achibbuisness512@gmail.com>
2024-06-23 23:17:56 +0100

    Refactor load_data.py
     - Update load_data.py to add comments to its functions, provide
       descriptions, and include type hints

--- src/utils/data/load_data.py
+++ src/utils/data/load_data.py
@@ -1,14 +1,33 @@
 import pickle
 import streamlit as st
+from typing import List, Any
 
 
 @st.cache_data
-def load_pickle_file(file_path: str):
+def load_pickle_file(file_path: str) -> Any:
+    """
+    Loads a pickle file from the given file path.
+
+    Parameters:
+    file_path (str): The path to the pickle file.
+
+    Returns:
+    Any: The data loaded from the pickle file.
+    """
     with open(file_path, "rb") as f:
         return pickle.load(f)
 
 
 @st.cache_data
-def load_vocab(file_path: str):
+def load_vocab(file_path: str) -> List[str]:
+    """
+    Loads a vocabulary from a file. Each line in the file is considered a word in the vocabulary.
+
+    Parameters:
+    file_path (str): The path to the file containing the vocabulary.
+
+    Returns:
+    List[str]: The list of words in the vocabulary.
+    """
     with open(file_path, "r") as f:
         return [line.strip() for line in f]
